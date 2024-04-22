9ad7e2f4f94698391bb11751a4ed9ca9dfc69ba3
winston-majano <winstonmajano@gmail.com>
2024-04-22 02:15:02 +0200

    subiendo ejemplo de consumo de eventos desde la api

---
+++ src/components/NewsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <div class="card mb-3">
+            <!--  <img src="${noticia.imagen || 'ruta_por_defecto_si_no_hay_imagen'}" class="card-img-top" alt="Imagen de la Noticia"> -->
+            <div class="card-body">
+                <h5 class="card-title">Titulo de la noticia</h5>
+                <p class="card-text">Descripcion de noticia </p>
+                <p>Fecha de noticia</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,7 +1,21 @@
 <script setup>
-
+import EventoComponent from '@/components/EventoComponent.vue'
+import NewsComponent from '@/components/NewsComponent.vue'
 </script>
 
 <template>
   <h1>Contenido de la vista principal</h1>
+ 
+<div class="container">
+  <div class="row">
+    <div class="col-8">
+       <EventoComponent/>
+    </div>
+    <div class="col-4">
+      <NewsComponent/>
+    </div>
+  </div>
+</div>
+
+
 </template>
