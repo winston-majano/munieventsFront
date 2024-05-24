48f5425f7e276522a830a3270888951d073f543f
winston-majano <winstonmajano@gmail.com>
2024-05-24 09:43:18 +0200

    listando eventos en mis eventos

--- src/components/EventoComponent.vue
+++ src/components/EventoComponent.vue
@@ -1,32 +1,52 @@
 <template>
-      <div class="col-md-12 mb-4" v-for="event in events" :key="event.id">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-md-4">
-                            <img :src='event.photo' class="card-img img-fluid" > 
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{event.title}}</h5>
-                                    <p class="card-text">{{event.description}}</p>
-                                    <div class="valoracion">valoracion</div>
-                                    <a href="#" class="btn btn-primary">Ver más</a>
-                                </div>
-                            </div>
-                        </div>
+    <div class="col-md-12 mb-4" v-for="event in events" :key="event.id">
+        <div class="card">
+            <div class="row">
+                <div class="col-md-4">
+                    <img :src='event.photo' class="card-img img-fluid">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ event.title }}</h5>
+                        <p class="card-text">{{ event.description }}</p>
+                        <a href="#" class="btn btn-primary">Ver más</a>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
 
 </template>
 
 
 <script setup>
 import { ref } from 'vue';
-const events = ref({});
-fetch('http://localhost:8080/api/v1/events')
-    .then(response => response.json())
-    .then(data => events.value = data);
+const events = ref(null);
+//fetch('http://localhost:8080/api/v1/events')
+//    .then(response => response.json())
+//    .then(data => events.value = data);
 
 //console.log(events.titulo_imagen);
-</script>
 
+const getEvents = async () => {
+
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/events`, {
+            method: 'GET'
+        });
+        console.log("Obteniendo lista de nombre eventos: ", response)
+        events.value = await response.json();
+        console.log("Respuesta: ", events.value)
+
+
+    } catch (error) {
+        console.log("Error exception: ", error.message)
+
+    }
+}
+
+//llamando a categorias 
+getEvents();
+
+
+</script>
