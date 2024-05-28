38c666f82ce0a0c2d08664aa531d24352cec692f
deakamm <deakamm2@gmail.com>
2024-05-28 22:01:37 +0200

    review vista detailEvento

--- src/components/EventDetailComponent.vue
+++ src/components/EventDetailComponent.vue
@@ -1,39 +1,37 @@
 <template>
-    <div class="col-md-12 mb-4" v-for="event in events" :key="event.id">
-        <div class="card h-100">
-            <img :src='event.photo' class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">{{ event.title }} </h5>
-                <p class="card-text">{{ event.description }}</p>
-                <p class="card-text_start">Fecha inicio: {{ event.start_date }}</p>
-                <p class="card-text_end">Fecha final: {{ event.end_date }}</p>
-                <p class="card-text">Ubication: {{ event.location }}</p>
-                <p class="card-text">Precio: {{ event.price }} €</p>
-                <p class="card-text">Valoracion: {{ event.coins }}</p>
-            </div>
+    <div class="col-md-12 mb-4" v-if="events">
+      <div class="card h-100">
+        <img :src="events.photo" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">{{ events.title }}</h5>
+          <p class="card-text">{{ events.description }}</p>
+          <p class="card-text_start">Fecha inicio: {{ events.start_date }}</p>
+          <p class="card-text_end">Fecha final: {{ events.end_date }}</p>
+          <p class="card-text">Ubicación: {{ events.location }}</p>
+          <p class="card-text">Precio: {{ events.price }} €</p>
+          <p class="card-text">Valoración: {{ events.coins }}</p>
         </div>
+      </div>
     </div>
-</template>
-
-
-<script setup>
-import { ref } from 'vue';
-
-const events = ref([]);;
-const evento = ref([]);
-
-fetch('http://localhost:8080/api/v1/events')
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+  const events = ref([]);
+  const eventId = ref(route.params.id);
+  const event = ref(null);
+  
+  fetch(`http://localhost:8080/api/v1/events/${eventId.value}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        events.value = data;
-        evento.value = data.find(event => event.id === 'id');
-        console.log(evento.value);
-        // if (evento.value) {
-        //     events.value = [0];
-        // }
-    });
-</script>
+      events.value = data;
+      console.log("lista evento id" ,data);
+      event.value = events.value.find(evento => evento.id === eventId.value);
+    })
+  </script>
 
 <style scoped>
 .card-img-top {
@@ -47,7 +45,6 @@ h5 {
     place-items: center;
 }
 
-#start,
 p {
     display: flex;
     justify-content: space-around;
--- src/components/EventoComponent.vue
+++ src/components/EventoComponent.vue
@@ -1,42 +1,44 @@
 <template>
-    <div class="col-md-12 mb-4" v-for="event in events" :key="event.id">
-        <div class="card">
-            <div class="row">
-                <div class="col-md-4">
-                    <img :src='event.photo' class="card-img img-fluid">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ event.title }}</h5>
-                        <p class="card-text">{{ event.description }}</p>
-                        <div class="valoracion">valoracion</div>
-                        <!-- <a :href="'/detailEvento' + event.id" class="btn btn-primary">Ver mas</a> -->
-                        <router-link :to="'/detailEvento/' + event.id" class="btn btn-primary">Ver más</router-link>
-                    </div>
-                </div>
-
-            </div>
+  <div class="col-md-12 mb-4" v-for="event in events" :key="event.id">
+    <div class="card">
+      <div class="row">
+        <div class="col-md-4">
+          <img :src="event.photo" class="card-img img-fluid" />
         </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{{ event.title }}</h5>
+            <p class="card-text">{{ event.description }}</p>
+            <div class="valoracion">valoracion</div>
+            <!-- <a :href="'/detailEvento' + event.id" class="btn btn-primary">Ver mas</a> -->
+            <router-link 
+              :to="'/detailEvento/' + event.id"
+              class="btn btn-primary"
+              >Ver más</router-link
+            >
+          </div>
+        </div>
+      </div>
     </div>
-
+  </div>
 </template>
 
-
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const show = ref([]);
 const events = ref({});
-fetch('http://localhost:8080/api/v1/events')
-    .then(response => response.json())
-    .then(data => events.value = data);
+fetch("http://localhost:8080/api/v1/events")
+  .then((response) => response.json())
+  .then((data) => (events.value = data));
 
 //console.log(events.titulo_imagen);
 </script>
 
 <style>
-.card-img, .card-img-bottom, .card-img-top {
-    width: 100%;
-    height: 100%;
+.card-img,
+.card-img-bottom,
+.card-img-top {
+  width: 100%;
+  height: 100%;
 }
 </style>
